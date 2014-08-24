180219b390f9737180b82b7cc31c4850274072e0
Quintin Adam <QuintinAdam@live.com>
2014-08-23 22:50:26 -0600

    adds more zombie changes

--- goz.rb
+++ goz.rb
@@ -1,10 +1,14 @@
 # Game of Life, Death and Zombies.
 
-# if a live cell has less of equal to 1 live neighbors or greater then 4 or more live neighbors the cell dies
-# if a dead cell has exactly 3 live neighbors the cell becomes alive
-# if a zombie cell has 3 or more live cells, zombie cell will die.
-# if zombie cell has less then 3 live cells it will convert all live cells around it to a zombie.
-# zombies can move if no live cells
+  # 1. if a live cell has less then or equal to 1 live neighbor or greater then 4 or more live neighbors the cell dies
+  # 2. if a dead cell has exactly 3 live neighbors the cell becomes alive
+  # 3. if a zombie cell has 3 or more live cells, zombie cell will die
+  # 4. if zombie cell has less then 3 live cells it will convert all live cells around it to a zombie.
+  # 5. if zombie cell has no live neighbors the zombie can move in any direction
+  # Game ends when:
+  #   - Zombies kill all the live cells
+  #   - All Zombies are dead
+  #   - All Alive cells are dead
 
 # get the screen size
 require 'io/console'
@@ -162,5 +166,12 @@ class String
 end
 
 #screen hight width, population, sleep, starting zombies.
-Game.new($screen.first / 2 , $screen.last / 2, 0.2, 0.1, 1).cycle!
+Game.new($screen.first / 2 , $screen.last / 2, 0.2, 0.1, 2).cycle!
+# Game.new( hight, width, % chance of live cell, sleep time, zombies at start of game).cycle!
 # Game.new.cycle!
+
+# - hight: Any Integer
+# - width: Any Integer
+# - % chance of live cell: From 0.1 to 1
+# - sleep time: Any Integer or float(higher the number the slower the program) 
+# - zombies at start of game: Any Integer
